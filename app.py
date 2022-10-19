26c7f1a262e192a2106b9795f9c66827786bc62d
joyienjoy <92083624+joyienjoy@users.noreply.github.com>
2022-10-19 19:04:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,17 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def home():
     return 'Hello from Flask & Docker'
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+@app.route('/one')
+def one():
+    return 'Hello from Flask & Docker - Page 1'
+
+@app.route('/two')
+def two():
+    return 'Hello from Flask & Docker - Page 2'
+
+@app.route('/one/first')
+def onefirst():
+    return 'Hello from Flask & Docker - Page 1 - Page 1st'
