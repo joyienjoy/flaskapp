c0ae772a2fd9bf7acbf4b75ee7a95e6c4ec16d1b
joyienjoy <92083624+joyienjoy@users.noreply.github.com>
2022-10-20 12:46:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,3 +16,6 @@ def two():
 @app.route('/one/first')
 def onefirst():
     return 'Hello from Flask & Docker - Page 1 - Page 1st'
+
+if __name__ == '__main__':
+        app.run(host='0.0.0.0', debug=True, port=5000)
